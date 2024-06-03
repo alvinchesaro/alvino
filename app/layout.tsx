98b06e2460ac77d6ee5e8aa0b09649b243b2e6f1
alvinchesaro <alvinchesaro@outlook.com>
2024-06-03 15:18:07 +0300

    Cursor and Grainy Effect Set Up

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import { Bricolage_Grotesque, Inter, Oswald  } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import GrainEffect from "@/components/visualEffects/grain-effect";
+import { Cursor } from "@/components/cursor/cursor";
 
 const MainFont = Bricolage_Grotesque({ subsets: ["latin"] });
 const OswaldFont = Oswald({ subsets: ["latin"], variable: "--font-oswald" });
@@ -11,6 +13,7 @@ const GravaFont = localFont({
   variable: "--font-grava",
 });
 
+// Metadata
 export const metadata: Metadata = {
   title: "Alvin Chesaro",
   description: "Alvin is a guy who is really passionate about Software Engineering and Design",
@@ -31,6 +34,8 @@ export default function RootLayout({
         MainFont.className, 
         OswaldFont.variable, 
         GravaFont.variable)}>
+          <GrainEffect/>
+          <Cursor color="#fff" />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,10 @@ import { clsx } from "clsx";
 import { cn } from "@/lib/utils";
 export default function Home() {
   return (
-   <div>
-      <h1 className={cn("text-4xl")}>
-        Hello my friendly World!
-      </h1>
-      <h1 className={cn("text-4xl font-oswald")}>
-        Hello my friendly World!
-      </h1>
-      <h1 className={cn("text-4xl font-grava")}>
-        Hello my friendly World!
-      </h1>
+   <div className="p-32">
+      <div className="link text-white text-2xl">
+        Hover Here
+      </div>
    </div>
   );
 }
