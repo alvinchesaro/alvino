2237d315b2a2c0da2a6680cdd419dce7ff3f393f
alvinchesaro <alvinchesaro@outlook.com>
2024-06-05 07:09:04 +0300

    Input and Textarea Component Set Up

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import WaterWaveWrapper from "@/components/visualEffects/water-wave-effect";
 import { FiHome } from "react-icons/fi";
 import { RiUser5Fill } from "react-icons/ri";
 import { FcGoogle } from "react-icons/fc";
+import Input from "@/components/ui/input";
+import { PiUserCircleFill } from "react-icons/pi";
+import TextArea from "@/components/ui/text-area";
+import { MdComment } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -34,6 +38,10 @@ export default function Home() {
             Google It
           </Button>
         </div>
+        <Input type="text" placeholder="Full Name" />
+        <Input type="text" placeholder="Full Name" icon={ <PiUserCircleFill /> } />
+        <TextArea placeholder="Comment" />
+        <TextArea placeholder="Comment" icon={ <MdComment /> } />
       </Card>
     </div> 
     </div> 
